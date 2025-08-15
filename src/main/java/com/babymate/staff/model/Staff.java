cade1d2c0cd118731f595eb78db975c91423f1ad
Douglas <douglas.chuang@gmail.com>
2025-08-15 14:48:52 +0800

    getter / setter finished

--- src/main/java/com/babymate/staff/model/Staff.java
+++ src/main/java/com/babymate/staff/model/Staff.java
@@ -4,6 +4,21 @@ public class Staff {
 	private String staffName;
 	
 	public static void main (String[] args) {
-		System.out.println("Hello Git");
+		Staff staff = new Staff("emp1");
+		
+		System.out.println("Hello Git and "+ staff.getStaffName());
+	}
+
+	public Staff(String staffName) {
+		super();
+		this.staffName = staffName;
+	}
+
+	public String getStaffName() {
+		return staffName;
+	}
+
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
 	}
 }
